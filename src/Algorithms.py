fc5b5d1cfe406bc9452b0f421387a917fd240882
Olivier Chen <56003401+ochen0124@users.noreply.github.com>
2020-08-19 23:18:22 -0400

    Update Algorithms.py
    BFS

--- src/Algorithms.py
+++ src/Algorithms.py
@@ -73,6 +73,18 @@ def depthfirstsearch(graph, startNode, endNode):
     return path
 
 
+def breadthfirstsearch(graph, startNode, endNode):
+    queue = [(startNode, [startNode])]
+    while queue:
+        (vertex, path) = queue.pop(0)
+        for next in graph[vertex] - set(path):
+            if next == endNode:
+                yield path + [next]
+            else:
+                queue.append((next, path + [next]))
+    return path
+
+
 def dijkstras(graph, startNode, endNode):
     distance = dict()
     distance[startNode] = 0
